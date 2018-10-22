5f523b289a8c50a6008bc68c1c5aeffa788c8587
dsvistunov <d.svistuno0v1991@gmail.com>
2018-10-22 18:44:42 +0300

    added Question, Answer models and migrations

--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(max_length=255)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    type = models.CharField(max_length=14)
+    text = models.CharField(max_length=120)
+    votes = models.IntegerField(default=0)
